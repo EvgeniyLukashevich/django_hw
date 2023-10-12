77980f40432c667d68610bb16e7474b504321508
EvgeniyLukashevich <humansyndr8me@gmail.com>
2023-10-13 01:30:15 +0800

    the 3rd seminar homework added

--- shop_project/shop_app/urls.py
+++ shop_project/shop_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('homework3/', views.show_customers, name='homework3'),
-    path('orders/<int:customer_id>/', views.week_orders, name='week_order')
+    path('week_orders/<int:customer_id>/', views.week_orders, name='week_orders'),
+    path('month_orders/<int:customer_id>/', views.month_orders, name='month_orders'),
+    path('year_orders/<int:customer_id>/', views.year_orders, name='year_orders'),
 ]
--- shop_project/shop_app/views.py
+++ shop_project/shop_app/views.py
@@ -11,8 +11,22 @@ def show_customers(request):
     customers_list = Customer.objects.all()
     return render(request, 'shop_app/homework3.html', {'customers': customers_list})
 
+
 def week_orders(request, customer_id):
     now = timezone.now()
     one_week_ago = now - timezone.timedelta(days=7)
     orders = Order.objects.filter(date_ordered__gte=one_week_ago, client=customer_id)
+    return render(request, 'shop_app/customer_orders.html', {'orders': orders})
+
+
+def month_orders(request, customer_id):
+    now = timezone.now()
+    one_month_ago = now - timezone.timedelta(days=30)
+    orders = Order.objects.filter(date_ordered__gte=one_month_ago, client=customer_id)
+    return render(request, 'shop_app/customer_orders.html', {'orders': orders})
+
+def year_orders(request, customer_id):
+    now = timezone.now()
+    one_year_ago = now - timezone.timedelta(days=365)
+    orders = Order.objects.filter(date_ordered__gte=one_year_ago, client=customer_id)
     return render(request, 'shop_app/customer_orders.html', {'orders': orders})
