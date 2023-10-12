0617a05e4c9b645ed485a772a913599d9856f05b
EvgeniyLukashevich <humansyndr8me@gmail.com>
2023-10-12 23:30:23 +0800

    init

---
+++ shop_project/shop_app/management/commands/fill-db.py
@@ -0,0 +1,78 @@
+from django.core.management.base import BaseCommand
+from shop_app.models import Customer, Product, Order, OrderItem
+from random import randint, choice, uniform
+
+firstnames = [
+    'James', "Ivan", "Emma", "Mary", "Stuart", "Philipp",
+    'Kate', "John", "Julia", "Dennis", "Markus", "Ariana",
+    'Ann', "Maria", "Christy", "Sofia", "Nickolas", "Robert",
+    'Robert', 'Lisa', 'Julia', 'Frank', 'Erica', 'Steven'
+]
+lastnames = [
+    'Wilson', 'Thuram', 'Pires', 'Hernandes', 'White', 'Gonzales',
+    'Ramsey', 'Shakiri', 'Gerrard', 'Terry', 'Rooney', 'McKolmack',
+    'Ballotelli', 'Benzema', 'Lahm', 'Alaba', 'Martial', 'Gates',
+    'Bergkamp', 'Shaw', 'Zanetti', 'Gatusso', 'Cole', 'Viera'
+]
+
+product_first_word = [
+    'Nice', 'Good', 'Perfect', 'Awesome', 'Great', 'Best',
+    'White', 'Black', 'Red', 'Yellow', 'Pink', 'Blue',
+    'Amazing', 'Cool', 'Violet', 'Incredible', 'Purple', 'Magenta'
+]
+
+product_second_word = [
+    'Jacket', 'Jeans', 'Shirt', 'Socks', 'Sneakers', 'Hat',
+    'Cap', 'Pants', 'Shorts', 'Shoes', 'Hoodie', 'Sweatshirt',
+    'Coat', 'Gloves', 'Underwear', 'Glasses', 'Pillow', 'Blanket'
+]
+
+
+class Command(BaseCommand):
+    help = 'Fill the database with fake data.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('number_of_users', type=int, help='Number of added users.')
+        parser.add_argument('number_of_products', type=int, help='Number of added products.')
+
+    def handle(self, *args, **kwargs):
+        order: Order
+        number_of_users = kwargs['number_of_users']
+        number_of_products = kwargs['number_of_products']
+        for i in range(1, number_of_users + 1):
+            firstname = choice(firstnames)
+            lastname = choice(lastnames)
+            customer = Customer(
+                name=f'{firstname} {lastname}',
+                email=f'{firstname.lower()}_{lastname.lower()}@gmail.kz',
+                password=f'secret_word_#{i}',
+                age=randint(16, 128)
+            )
+            customer.save()
+        for i in range(1, number_of_products + 1):
+            product = Product(
+                name=f'{choice(product_first_word)} {choice(product_second_word)}',
+                description=f'Long description #{i}',
+                price=uniform(100, 100000),
+                quantity=randint(10, 100)
+            )
+            product.save()
+        for i in range(1, Customer.objects.count() + 1):
+            if randint(0, 3):
+                price = 0
+                order = Order(client=Customer.objects.filter(pk=i).first(), total_price=price)
+                order.save()
+                products_list=[]
+                for _ in range(1, randint(3, 11)):
+                    product = Product.objects.filter(pk=randint(1, Product.objects.count())).first()
+                    if product in products_list:
+                        continue
+                    products_list.append(product)
+                    order_item = OrderItem(order=order, product=product, quantity=randint(1, 5))
+                    order_item.save()
+                    price += product.price * order_item.quantity
+                    order.products.add(product)
+                    product.quantity -= 1
+                    product.save()
+                order.total_price = price
+                order.save()
---
+++ shop_project/shop_app/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
+    age = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='OrderItem')
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        order_details = []
+        order_details.append(f"Покупатель: {self.client.name}")
+        order_details.append("Товары:")
+        for product in self.products.all():
+            order_details.append(f"- {product.name} - Цена: {product.price}")
+        order_details.append(f"Итоговая сумма: {self.total_price}")
+        return "\n".join(order_details)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
